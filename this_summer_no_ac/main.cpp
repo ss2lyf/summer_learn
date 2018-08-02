85df1d2eb89464eea97f7d147bebbcc3a049dfee
Simon <ss2lyf@outlook.com>
2018-08-02 09:37:15 +0800

    initial commit

--- this_summer_no_ac/main.cpp
+++ this_summer_no_ac/main.cpp
@@ -4,15 +4,17 @@
 using namespace std;
 
 //也不是找性价比，也不是找第一个，需要找的是结束的时间
-typedef struct tv{
+typedef struct tv
+{
 
-int begin_time;
-int end_time;
+    int begin_time;
+    int end_time;
 
-}tv;
+} tv;
 
-bool cmp(tv a,tv b){
-return a.end_time<b.end_time;
+bool cmp(tv a,tv b)
+{
+    return a.end_time<b.end_time;
 }
 /*input
 
@@ -34,33 +36,34 @@ return a.end_time<b.end_time;
 int main()
 {
     int num,i;
- int time_now;
+    int time_now;
 
-int count_num;
+    int count_num;
     while((cin>>num)&&num!=0)
     {
         vector <tv> a(num);
-time_now=0;
-count_num=0;
-        for(i=0;i<num;i++)
+        time_now=0;
+        count_num=0;
+        for(i=0; i<num; i++)
         {
             cin>>a[i].begin_time>>a[i].end_time;
         }
 
         sort(a.begin(),a.end(),cmp);
         i=0;
-while(i<num)
-{
-    if(a[i].begin_time>=time_now)
-    {
-        count_num++;
-        time_now=a[i].end_time;
-    }
-    i++;
-}
+        while(i<num)
+        {
+            if(a[i].begin_time>=time_now)
+            //注意时间的衔接
+            {
+                count_num++;
+                time_now=a[i].end_time;
+            }
+            i++;
+        }
 
-cout<<count_num<<endl;
-a.clear();
+        cout<<count_num<<endl;
+        a.clear();
     }
     return 0;
 }
