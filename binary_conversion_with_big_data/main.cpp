56c4c932b09a12f172b027b98513b46e945c9030
Simon <ss2lyf@outlook.com>
2018-08-04 16:41:30 +0800

    initial commit

--- binary_conversion_with_big_data/main.cpp
+++ binary_conversion_with_big_data/main.cpp
@@ -5,6 +5,7 @@
 #include<stack>
 using namespace std;
 stack<char> s;
+//写完感觉整个人都不好了
 typedef struct bigint
 {
     int digit[1000];
@@ -110,101 +111,177 @@ bigint compute_bigint_plus(bigint a,bigint b)
                 a.digit[i]=middle;
             }
         }
-
-
         else
         {
 
             break;
         }
-
     }
     if(f==1)//大于最大的数的长度的仅为
     {
         a.digit[i]=1;
         a.int_size++;
     }
-
-  return a;
+    return a;
 
 }
 
-bigint compute_bigint_mul(bigint b,int a,)
+bigint compute_bigint_mul(bigint b,int a)
 {
-int i,j;
-int f=0;
-int middle;
-bigint temp,result;
-int temp_point;
-make_bigint( "0",&result);
-make_bigint( "0",&temp);
+    int i,j;
+    int f=0;
+    int middle;
+    bigint temp,result;
+    int temp_point;
+    make_bigint( "0",&result);
+    make_bigint( "0",&temp);
     //for(i=0;i<a.int_size;i++)
-    {temp.int_size=0;
+    {
+        temp.int_size=0;
 
         //cout<<a_temp<<endl;
-        for(j=0;j<b.int_size;j++)
+        for(j=0; j<b.int_size; j++)
         {
             middle=f+a*b.digit[j];
             f=middle/10000;
             if(f)
             {
-           temp.digit[temp.int_size]=middle%10000;
-            }else{
-
-           temp.digit[temp.int_size]=middle;
+                temp.digit[temp.int_size]=middle%10000;
+            }
+            else
+            {
+                temp.digit[temp.int_size]=middle;
             }
             temp.int_size++;
         }
         if(f>0)
         {
-             temp.digit[temp.int_size]=f;
-             temp.int_size++;
-
+            temp.digit[temp.int_size]=f;
+            temp.int_size++;
         }
         result=compute_bigint_plus(result,temp);
-        }
+    }
+    return result;
 
-return result;
+}
 
+bigint compute_bigint_div(bigint a,int b)
+{
+    int middle;
+    int r=0;
+    int temp;
+    int i;
+
+    for(i=a.int_size-1; i>=0; i--)
+    {
+        temp=a.digit[i]+r*10000;
+        r=temp%b;
+        middle=temp/b;
+        a.digit[i]=middle;
     }
 
+    if(a.digit[a.int_size-1]==0&&a.int_size!=1)//一个不大于36的数撑死把第一位除空吧
+    {
+        a.int_size--;
+    }
+    return a;
+
+}
+int compute_bigint_mod(bigint a,int b)
+{
+    int middle;
+    int r=0;
+    int temp;
+
+    int i;
+
+    for(i=a.int_size-1; i>=0; i--)
+    {
+        temp=a.digit[i]+r*10000;
+        r=temp%b;
+        middle=temp/b;
+        a.digit[i]=middle;
+    }
+
+    if(a.digit[a.int_size-1]==0&&a.int_size!=1)//一个不大于36的数撑死把第一位除空吧
+    {
+        a.int_size--;
+    }
+    return r;
+
+}
 string  int_to_string(int a)
 {
-   char middle;
-   string str;
+    char middle;
+    string str;
     do
     {
-middle=(a%10)+'0';
-s.push(middle);
-
+        middle=(a%10)+'0';
+        s.push(middle);
         a=a/10;
-    }while(a!=0);
+    }
+    while(a!=0);
 
     while(!s.empty())
     {
         str=str+s.top();
         s.pop();
     }
-
-return str;
-
-
+    return str;
 }
 
+void init(bigint & a)
+{
+a.digit[0]=0;
+    a.int_size=1;
+
+}
 int main()
 {
-    int a;
-    string a_temp;
-    bigint a_i;
-    bigint result;
-     cin>>a;
-    make_bigint("1",&result);
-    a_temp=int_to_string(a);
-    //cout<<a_temp;
-    make_bigint(a_temp,&a_i);
-    result=compute_bigint_mul(99,a_i);
-
-print_bigint(result);
-
+    int a,b,i;
+    string str;
+    int result;
+    bigint temp,temp1,temp2;
+    init(temp);
+    cin>>a>>b>>str;
+    temp2.digit[0]=1;
+    temp2.int_size=1;
+    for(i=str.size()-1; i>=0; i--)
+    {
+        if(str[i]<'A')
+        {
+            temp1=compute_bigint_mul(temp2,str[i]-'0');
+        }
+        else
+        {
+            temp1=compute_bigint_mul(temp2,str[i]-'A'+10);
+        }
+        temp=compute_bigint_plus(temp,temp1);
+        temp2=compute_bigint_mul(temp2,a);
+    }
+    print_bigint(temp);
+    while(true)
+    {
+        result=compute_bigint_mod(temp,b);
+        if(result<10)
+        {
+            s.push(result+'0');
+        }
+        else
+        {
+            s.push(result-10+'a');
+        }
+        temp=compute_bigint_div(temp,b);
+        if(temp.digit[0]==0&&temp.int_size==1)
+        {
+            break;
+        }
+    }
+    while(!s.empty())
+    {
+        cout<<s.top();
+        s.pop();
+    }
+    cout<<endl;
     return 0;
 }
